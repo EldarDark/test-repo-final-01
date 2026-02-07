31aa7118c9ea6072acb8139fffb259b117d6ca37
DarkStar <155805083+EldarDark@users.noreply.github.com>
2026-02-07 15:29:27 +0530

    AI Gen: Update blocks/columns/columns.js

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -2,17 +2,12 @@ export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
-  // setup image columns
+  // Identify media vs text
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
       const pic = col.querySelector('picture');
-      if (pic) {
-        const picWrapper = pic.closest('div');
-        if (picWrapper && picWrapper.children.length === 1) {
-          // picture is only content in column
-          picWrapper.classList.add('columns-img-col');
-        }
-      }
+      if (pic) col.classList.add('columns-img-col');
+      else col.classList.add('columns-text-col');
     });
   });
 }
